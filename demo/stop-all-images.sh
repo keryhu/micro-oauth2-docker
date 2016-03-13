0fb0735757b7015488d0f7f0571d960851307b85
keryhu <keryhu@hotmail.com>
2016-03-13 13:56:54 +0800

    update rm container

--- demo/stop-all-images.sh
+++ demo/stop-all-images.sh
@@ -6,6 +6,10 @@
  #删除所有的在运行的container 进程
  docker rm $(docker ps -a -q -f status=exited)
 
-#删除所有不用的docker volume
+#删除所有带can-autoremove label的docker volume
 sleep 5
 docker volume rm $(docker volume ls -qf dangling=true)
+
+#删除所有带有 can-autoremove 标签的images
+sleep 5
+docker rmi $(docker images -q -f "label=can-autoremove=true")
